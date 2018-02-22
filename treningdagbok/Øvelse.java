622e2ace823b6ceaae292a91f1c1efc0efe913cb
Kenneth Gabrielsen <kennethgabrielsen@hotmail.com>
2018-02-22 10:52:57 +0000

    fiksa skitet til robert

--- treningdagbok/Øvelse.java
+++ treningdagbok/Øvelse.java
@@ -21,11 +21,7 @@ public class Øvelse extends ActiveDomainObject {
 	public void save(Connection conn) {
 		try {
 			Statement stmt = conn.createStatement();
-<<<<<<< HEAD
 			Resultset rs = stmt.executeQuery("update Øvelse set navn="+navn+", beskrivelse="+beskrivelse+", apparatid="+apparatid+"where øvelsesid="+øvelsesid);
-=======
-			Resultset rs = stmt.executeQuery("update Øvelse set øvelsenavn="+navn+", øvelsebeskrivelse="+beskrivelse+", apparatid="+apparatid+"where øvelsesid="+øvelsesid);
->>>>>>> 7ee6afb9e47e77079fc42b4a6d3eaa994df2cbac
 		} catch(Exception e) {
 			System.out.println("db error during update of øvelse="+e);
 			return;
@@ -38,19 +34,11 @@ public class Øvelse extends ActiveDomainObject {
 	public void initialize(Connection conn) {
 		try {
 			Statement stmt = conn.createStatement();
-<<<<<<< HEAD
 			ResultSet rs = stmt.executeQuery("select navn, beskrivelse, apparatid from Øvelse where øvelsesid="+øvelsesid);
 			while(rs.next()) {
 				navn = rs.getString("navn");
 				beskrivelse = rs.getString("beskrivelse");
 				apparatid = rs.getString("apparatid");
-=======
-			ResultSet rs = stmt.executeQuery("select øvelsenavn, øvelsebeskrivelse, apparatid from Øvelse where øvelsesid="+øvelsesid);
-			while(rs.next()) {
-				navn = rs.getString("øvelsenavn");
-				beskrivelse = rs.getString("øvelsebeskrivelse");
-				apparatid = rs.getInt("apparatid");
->>>>>>> 7ee6afb9e47e77079fc42b4a6d3eaa994df2cbac
 			}
 		} catch( Exception e) {
 			System.out.println("db error during select of øvelse= "+e);
--- treningdagbok/ØvelsesGruppe.java
+++ treningdagbok/ØvelsesGruppe.java
@@ -5,27 +5,16 @@ import java.util.*;
 
 public class ØvelsesGruppe extends ActiveDomainObject {
 	private int gruppeid;
-<<<<<<< HEAD
-	private int String beskrivelse;
-
-	public ØvelsesGruppe(int gruppeid) {
-		this.gruppeid = gruppeid
-=======
 	private String beskrivelse;
 
 	public ØvelsesGruppe(int gruppeid) {
 		this.gruppeid = gruppeid;
->>>>>>> 7ee6afb9e47e77079fc42b4a6d3eaa994df2cbac
 	}
 
 	public void initialize(Connection conn) {
 		try {
 			Statement stmt = conn.createStatement();
-<<<<<<< HEAD
-			ResultSet rs = stmt.executeQuery("select beskrivelse from ØvelsesGruppe where gruppeid="+gruppeid);
-=======
 			ResultSet rs = stmt.executeQuery("select gruppebeskrivelse from ØvelsesGruppe where gruppeid="+gruppeid);
->>>>>>> 7ee6afb9e47e77079fc42b4a6d3eaa994df2cbac
 			while(rs.next()) {
 				beskrivelse = rs.getString("gruppebeskrivelse");
 			}
@@ -43,22 +32,15 @@ public class ØvelsesGruppe extends ActiveDomainObject {
 	public void save(Connection conn) {
 		try {
 			Statement stmt = conn.createStatement();
-<<<<<<< HEAD
-			ResultSet rs = stmt.executeQuery("update ØvelsesGruppe set beskrivelse="+beskrivelse+"where gruppeid="+gruppeid);
-=======
 			ResultSet rs = stmt.executeQuery("update ØvelsesGruppe set gruppebeskrivelse="+beskrivelse+"where gruppeid="+gruppeid);
->>>>>>> 7ee6afb9e47e77079fc42b4a6d3eaa994df2cbac
 			} catch(Exception e) {
 			System.out.println("db error during update of øvelsesgruppe="+e);
 			return;
 			}
 	}
 
-<<<<<<< HEAD
-=======
 }
 
->>>>>>> 7ee6afb9e47e77079fc42b4a6d3eaa994df2cbac
 
 
 
