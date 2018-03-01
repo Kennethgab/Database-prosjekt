aa413d0428285d42fa8f474cc3e4384b2daa3a21
Kennethgab <Kennethgabrielsen@hotmail.com>
2018-03-01 15:03:16 +0100

    added a bunch of memefiles

---
+++ treningdagbok/ActiveDomainObject.java
@@ -0,0 +1,12 @@
+package treningdagbok;
+
+
+import java.sql*;
+
+public abstract class ActiveDomainObject {
+	public abstract void initialize( Connection conn);
+	public abstract void refresh( Connection conn);
+	public abstract void save( Connection conn);
+}
+
+
