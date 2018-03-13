616f87222e41be675d5fab16a6f6f2b02e38486b
OmegaLLC <vitrovine@gmail.com>
2018-03-13 13:56:18 +0100

    Beauty

--- Trening/src/treningdagbok/Apparat.java
+++ Trening/src/treningdagbok/Apparat.java
@@ -1,16 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 package treningdagbok;
-
-/**
- *
- * @author sveinbra
- */
-
 import java.sql.*;
 import java.util.*;
 
