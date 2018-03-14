29e2318683a438fe09ee877d41a33c907c8e788a
OmegaLLC <vitrovine@gmail.com>
2018-03-14 13:56:35 +0100

    Changes

--- Trening/src/treningdagbok/TreningsDagbokController.java
+++ Trening/src/treningdagbok/TreningsDagbokController.java
@@ -9,28 +9,36 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 public class TreningsDagbokController {
-
+	//new apparat
 	@FXML private Button newApparatButton;
 	@FXML private TextField apparatIDText;
 	@FXML private TextField apparatNavnText;
 	@FXML private TextField apparatBeskrivelseText;
-	@FXML private TextArea apparatTextArea;
-
+	@FXML private TextArea debugTextArea;
+		//refresh
 	@FXML private Button updateButton;
-
+		//new øvelse
 	@FXML private Button newOvelseButton;
 	@FXML private TextField ovelseIDText;
 	@FXML private TextField ovelseNavnText;
 	@FXML private TextField ovelseBeskrivelseText;
 	@FXML private TextField ovelseApparatIDText;
-	
+		//get øvelser som bruker apparatid
 	@FXML private TextField getOvelserApparatID;
 	@FXML private Button getOvelserButton;
+		//new øvelsesgruppe
+	@FXML private TextField øvelsesgruppeIDText;
+	@FXML private TextField øvelsesgruppeBeskrivelseText;
+	@FXML private Button newØvelsesgruppeButton;
+	
+	@FXML private TextField getGrupperOvelseIDText;
+	@FXML private Button getGrupperButton;
 
 
 	private DBConn conn;
 	private Apparat a;
 	private Øvelse o;
+	private ØvelsesGruppe og;
 
 	@FXML
 	private void initialize() {
@@ -41,7 +49,6 @@ public class TreningsDagbokController {
 			System.out.println("Error connecting to database: "+e);
 			//e.printStackTrace();
 		}
-
 	}
 	
 	@FXML
@@ -51,7 +58,12 @@ public class TreningsDagbokController {
 		for (Øvelse o : list) {
 			output += "øvelsesid: " + o.getØvelsesid() + "\nnavn: " + o.getNavn() + "\nbeskrivelse: " + o.getBeskrivelse() + "\napparatid:" + o.getApparatid()+"\n\n";
 		}
-		apparatTextArea.setText(output);
+		debugTextArea.setText(output);
+	}
+	
+	@FXML public void getOvelserForGruppe() {
+		List<ØvelsesGruppe> list = SQLQuery.getOvelserInGroup(conn.conn, Integer.parseInt(getGrupperOvelseIDText.getText()));
+		debugList(list);
 	}
 
 	@FXML
@@ -88,8 +100,43 @@ public class TreningsDagbokController {
 		
 	}
 	
-	public void refresh() {
+	@FXML
+	public void newOvelsesGruppe() {
+		try {
+			ØvelsesGruppe og = new ØvelsesGruppe(Integer.parseInt(ovelseIDText.getText()));
+			this.og = og;
+			o.setNavn(apparatNavnText.getText());
+			o.setBeskrivelse(ovelseBeskrivelseText.getText());
+			o.setApparatid(Integer.parseInt(ovelseApparatIDText.getText()));
+			o.save(conn.conn);
+			System.out.println("update?");
+			refresh();
+			System.out.println("update done?");
+			} catch (Exception e) {
+				System.out.println("error med å lage ny øvelse: "+e);
+			}
+		
+	}
+	
+	public void debugObject(Object o) {
+		try {
+			this.debugTextArea.setText(o.toString());
+		}
+		catch (Exception e) {
+			
+		}
+	}
+	
+	public void debugList(List<?> l) {
 		String s = "";
+		for (Object o : l) {
+			s+=o.toString() + "\n\n";
+		}
+		this.debugTextArea.setText(s);
+	}
+	
+	public void refresh() {
+	 /*	String s = "";
 		try {
 			s += a.getApparatID()+"";
 			s += "\n"+a.getNavn();
@@ -107,7 +154,7 @@ public class TreningsDagbokController {
 			System.out.println("error updating o: "+e);
 			
 		}
-		this.apparatTextArea.setText(s);
+		this.debugTextArea.setText(s);*/
 		
 	}
 
