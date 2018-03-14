20d7e4f2f6a626684d2fcf2e4df61bdefaf2d7ee
OmegaLLC <vitrovine@gmail.com>
2018-03-14 10:40:45 +0100

    made SQLQuery class

---
+++ Trening/src/treningdagbok/SQLQuery.java
@@ -0,0 +1,32 @@
+package treningdagbok;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SQLQuery {
+	
+	public void getOvelserTilApparat (Connection conn, int apparatid) {
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select øvelsesid, øvelsesnavn, øvelsesbeskrivelse, apparatid from Øvelse WHERE apparatid=" + apparatid);
+            Øvelse o;
+            List<Øvelse> list = new ArrayList<Øvelse>();
+            while (rs.next()) {
+            	o = new Øvelse(rs.getInt("øvelsesid"));
+                o.setNavn(rs.getString("øvelsenavn"));
+                o.setBeskrivelse(rs.getString("øvelsebeskrivelse"));
+                o.setApparatid(rs.getInt("apparatid"));
+                list.add(o);
+            }
+
+        } catch (Exception e) {
+            System.out.println("db error during select of bruker= "+e);
+            return;
+        }
+
+    }
+
+}
